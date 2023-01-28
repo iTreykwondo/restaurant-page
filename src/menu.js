3f040d967aee7c38e7d7d9fdddfb17354536dc09
Trajan Miller <trajanmiller324@gmail.com>
2023-01-27 20:38:07 -0500

    Create menu component

---
+++ src/menu.js
@@ -0,0 +1,6 @@
+export const Menu = () => {
+  const mainContent = document.querySelector("#content");
+  const title = document.createElement("h1");
+  title.textContent = "Our Menu";
+  mainContent.appendChild(title);
+};
